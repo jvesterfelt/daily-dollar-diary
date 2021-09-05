a60107bbdbabd0e46bd968f908fa76e45bd24736
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-09-05 17:03:47 -0600

    made changes to service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,57 @@
+const APP_PREFIX = 'Daily-Dollar-Diary_';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
     "./js/idb.js",
     "./js/index.js",
-    "./css/style.css",
-    "../models/transaction.js",
-    "../routes/api.js",
-    "../server.js"
+    "./css/styles.css"
 ];
 
-const APP_PREFIX = 'Daily-Dollar-Diary_';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
-
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
-        })
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
     )
-});
+  })
 
-self.addEventListener('activate', function (e) {
+  self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeeplist.push(CACHE_NAME);
-
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheKeeplist.indexOf(key) === -1) {
-                    console.log('deleting cache: ' + keyList[i]);
-                    return caches.delete(keyList[i]);
-                }
-            }));
+      caches.keys().then(function (keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
         })
-    )
-});
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(keyList.map(function (key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i] );
+              return caches.delete(keyList[i]);
+            }
+        }));
+      })
+    );
+  });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request: ' + e.request.url);
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
     e.respondWith(
-        caches.match(e.request).then(function (request) {
-            // if (request) {
-            //     console.log('responding with cache: ' + e.request.url)
-            //     return request
-            // } else {       // if there are no cache, try fetching request
-            //     console.log('file is not cached, fetching : ' + e.request.url)
-            //     return fetch(e.request)
-            //   }
-            
-            return request || fetch(e.request);
-        })
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
     )
-});
+  })
